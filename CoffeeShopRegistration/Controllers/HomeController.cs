5c7f926b2e2f7a268177b66d2b645d6446438c43
Jon <JJKing619@gmail.com>
2022-03-04 12:34:21 -0500

    First

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationResult(string FirstName, string LastName, string email, string password)
+        {
+            ViewData["FirstName"] = FirstName;
+            ViewData["LastName"] = LastName;
+            ViewData["Email"] = email;
+            ViewData["Password"] = password;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
